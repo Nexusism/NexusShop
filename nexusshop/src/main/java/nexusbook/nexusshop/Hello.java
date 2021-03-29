ef701a3d588c55a89e17960c20054babc34c9117
Nexusism <wanggoog91@gmail.com>
2021-03-29 23:23:35 +0900

    20210329 shop 시작

---
+++ nexusshop/src/main/java/nexusbook/nexusshop/Hello.java
@@ -0,0 +1,9 @@
+package nexusbook.nexusshop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Hello {
+}
--- nexusshop/src/test/java/nexusbook/nexusshop/NexusshopApplicationTests.java
+++ nexusshop/src/test/java/nexusbook/nexusshop/NexusshopApplicationTests.java
@@ -1,5 +1,6 @@
 package nexusbook.nexusshop;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
