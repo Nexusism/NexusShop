a411d6e31c8d95c4828de2985949b8e8e5eb2a0e
Nexusism <wanggoog91@gmail.com>
2021-03-29 22:46:12 +0900

    20210328 NexusShop

---
+++ nexusshop/src/main/java/nexusbook/nexusshop/NexusshopApplication.java
@@ -0,0 +1,13 @@
+package nexusbook.nexusshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusshopApplication.class, args);
+	}
+
+}
---
+++ nexusshop/src/test/java/nexusbook/nexusshop/NexusshopApplicationTests.java
@@ -0,0 +1,13 @@
+package nexusbook.nexusshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
