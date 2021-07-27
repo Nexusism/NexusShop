929f6f5694dc281c546eb5c1dd50f5e7c6ba593e
Nexusism <wanggoog91@gmail.com>
2021-07-27 17:55:22 +0900

    OrderService TEST

--- nexusshop/src/main/java/nexusbook/nexusshop/repository/OrderRepository.java
+++ nexusshop/src/main/java/nexusbook/nexusshop/repository/OrderRepository.java
@@ -1,4 +1,24 @@
 package nexusbook.nexusshop.repository;
 
+import lombok.RequiredArgsConstructor;
+import nexusbook.nexusshop.domain.Order;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
 public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order){
+        em.persist(order);
+    }
+
+    public Order findOne(Long id){
+        return em.find(Order.class, id);
+    }
+
+//      public List<Order> findAll(OrderSearch orderSearch) { ... }
 }
---
+++ nexusshop/src/test/java/nexusbook/nexusshop/Service/OrderServiceTest.java
@@ -0,0 +1,35 @@
+package nexusbook.nexusshop.Service;
+
+import nexusbook.nexusshop.repository.OrderRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.junit.Assert.*;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class OrderServiceTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Autowired OrderService orderService;
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    public void 상품주문() throws Exception {
+
+    }
+
+}
