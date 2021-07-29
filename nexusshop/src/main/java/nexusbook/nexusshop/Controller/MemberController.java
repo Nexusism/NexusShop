29599c4cb38255c88894d62a38f8cd2f178462e1
Nexusism <wanggoog91@gmail.com>
2021-07-29 16:48:35 +0900

    20210729 - createMemberController

--- nexusshop/src/main/java/nexusbook/nexusshop/Controller/MemberController.java
+++ nexusshop/src/main/java/nexusbook/nexusshop/Controller/MemberController.java
@@ -2,10 +2,16 @@ package nexusbook.nexusshop.Controller;
 
 import lombok.RequiredArgsConstructor;
 import nexusbook.nexusshop.Service.MemberService;
+import nexusbook.nexusshop.domain.Address;
+import nexusbook.nexusshop.domain.Member;
 import nexusbook.nexusshop.web.MemberForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,5 +22,22 @@ public class MemberController {
     @GetMapping(value = "/members/new")
     public String createForm(Model model) {
         model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberFrom";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
